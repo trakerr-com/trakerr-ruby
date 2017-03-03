b09cd7458b5ffdc593dd7aa043712e97564a3f21
RM <rangers1500@yahoo.com>
2017-03-02 16:29:20 -0800

    Scrub personal details.

--- test_app.rb
+++ test_app.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require_relative 'trakerr/lib/trakerr'
 
 def main()
-    testApp = Trakerr::TrakerrClient.new("a56a68537730468def34067d0df7943f17815001900144", "1.0", "development")
+    testApp = Trakerr::TrakerrClient.new("Api Key Here", "1.0", "development")
     begin
         raise ArgumentError
     rescue Exception => e
